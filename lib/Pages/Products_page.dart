97f72cb26f578c0cc57f4291f901a9d6d073294d
Lemonioneater <142171270+Lemonioneater@users.noreply.github.com>
2023-08-25 09:18:14 +0300

    Update README.md

--- lib/Pages/Products_page.dart
+++ lib/Pages/Products_page.dart
@@ -19,14 +19,9 @@ Future<bool> logout() async{
 }
 
 
-class Products extends StatefulWidget {
+class Products extends StatelessWidget {
   const Products({super.key});
 
-  @override
-  State<Products> createState() => _ProductsState();
-}
-
-class _ProductsState extends State<Products> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
